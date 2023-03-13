0776ca0a69b09dbf2b0faaca45e8535c5f74234d
pulkitjuneja1997 <pulkitjuneja1997@gmail.com>
2023-03-13 13:45:43 +0530

    3 commit

--- api/prepareTemplate.php
+++ api/prepareTemplate.php
@@ -331,8 +331,61 @@ class Amazon_Integration_For_Woocommerce_Admin {
         $listname_of_all_tabs_files = $this->reader->listWorksheetNames($localFileName);
         $spreadsheet = $this->reader->load($localFileName);
 
+
+        // ----------------------------------------------------- VALID_VALUES.JSON ----------------------------------------------------------
+
+        $valid_values_key = array_search( 'Valid Values', $listname_of_all_tabs_files );
+        $valid_values = $listname_of_all_tabs_files[$valid_values_key];
+        
+        $highestColumnIndex = \PhpOffice\PhpSpreadsheet\Cell\Coordinate::columnIndexFromString($spreadsheet->getSheetByName($valid_values)->getHighestColumn());
+        $highestRow = $spreadsheet->getSheetByName($valid_values)->getHighestRow();
+        
+        $sectionHeading = '';
+        $valid_values_array = array();
+        $subCategory = '';
+  
+        for ($row = 1; $row <= $highestRow; ++$row) {
+            for ($col = 1; $col <= $highestColumnIndex; ++$col) {
+                if ($col == 2 ) {
+                    $label = $spreadsheet->getSheetByName($valid_values)->getCellByColumnAndRow(2, $row)->getValue();
+                    if ( !empty($label) ){
+                        if ( !empty($label) && strpos( $label, ' - ') !== false) {
+                            
+                            $labelArray = explode(' - ', $label);
+                            $label = $labelArray[0];
+                            $subCategory = $labelArray[1];
+                            $subCategory = ltrim( $subCategory,"[ " );
+                            $subCategory = rtrim( $subCategory," ]" );
+
+                            if( empty( $subCategory ) ) { $subCategory = 'all_cat'; }
+                            
+                        } else{ continue;}
+        
+                        $valid_values_array[$label] = array();
+                        $valid_values_array[$label][$subCategory] = array();
+        
+                    }
+
+                } else if( $col > 2) {
+
+                    $option = $spreadsheet->getSheetByName($valid_values)->getCellByColumnAndRow($col, $row)->getValue();
+                    if( !empty( $option ) ){
+                        $valid_values_array[$label][$subCategory][$option] = $option;
+                    }
+
+                }
+
+            }
+                    
+
+        }
+
+        // ----------------------------------------------------- VALID_VALUES.JSON ----------------------------------------------------------
+  
+
         // ----------------------------------------------------- PRODUCTS_TEMPLATE_FIELDS.JSON ----------------------------------------------------------
 
+        $sub_category_id = $subCategory;
         $products_template_fields_key = array_search( 'Data Definitions', $listname_of_all_tabs_files  );
         $products_template_fields = $listname_of_all_tabs_files[$products_template_fields_key];
 
@@ -412,7 +465,7 @@ class Amazon_Integration_For_Woocommerce_Admin {
                                     'definition' => $definitions_and_uses,
                                     'accepted_value' => $accepted_values,
                                     'example' => $examples,
-                                    'productTypeSpecific' => array( 'sub_category_id' => array( 'condition' => lcfirst( $required ) ) )
+                                    'productTypeSpecific' => array( $sub_category_id => array( 'condition' => lcfirst( $required ) ) )
                                 );
                             
 
@@ -425,7 +478,7 @@ class Amazon_Integration_For_Woocommerce_Admin {
                                     'definition' => $definitions_and_uses,
                                     'accepted_value' => $accepted_values,
                                     'example' => $examples,
-                                    'productTypeSpecific' => array( 'sub_category_id' => array( 'condition' => lcfirst( $required ) ) )
+                                    'productTypeSpecific' => array( $sub_category_id => array( 'condition' => lcfirst( $required ) ) )
                                 );
 
                             }
@@ -445,56 +498,7 @@ class Amazon_Integration_For_Woocommerce_Admin {
 
         // ----------------------------------------------------- PRODUCTS_TEMPLATE_FIELDS.JSON ----------------------------------------------------------
 
-        // ----------------------------------------------------- VALID_VALUES.JSON ----------------------------------------------------------
-
-        $valid_values_key = array_search( 'Valid Values', $listname_of_all_tabs_files );
-        $valid_values = $listname_of_all_tabs_files[$valid_values_key];
-        
-        $highestColumnIndex = \PhpOffice\PhpSpreadsheet\Cell\Coordinate::columnIndexFromString($spreadsheet->getSheetByName($valid_values)->getHighestColumn());
-        $highestRow = $spreadsheet->getSheetByName($valid_values)->getHighestRow();
-        
-        $sectionHeading = '';
-        $valid_values_array = array();
-        $subCategory = '';
-
-        for ($row = 1; $row <= $highestRow; ++$row) {
-            for ($col = 1; $col <= $highestColumnIndex; ++$col) {
-                if ($col == 2 ) {
-                    $label = $spreadsheet->getSheetByName($valid_values)->getCellByColumnAndRow(2, $row)->getValue();
-                    if ( !empty($label) ){
-                        if ( !empty($label) && strpos( $label, ' - ') !== false) {
-                            
-                            $labelArray = explode(' - ', $label);
-                            $label = $labelArray[0];
-                            $subCategory = $labelArray[1];
-                            $subCategory = ltrim( $subCategory,"[ " );
-                            $subCategory = rtrim( $subCategory," ]" );
-
-                            if( empty( $subCategory ) ) { $subCategory = 'all_cat'; }
-                            
-                        } else{ continue;}
-        
-                        $valid_values_array[$label] = array();
-                        $valid_values_array[$label][$subCategory] = array();
-        
-                    }
-
-                } else if( $col > 2) {
-
-                    $option = $spreadsheet->getSheetByName($valid_values)->getCellByColumnAndRow($col, $row)->getValue();
-                    if( !empty( $option ) ){
-                        $valid_values_array[$label][$subCategory][$option] = $option;
-                    }
-
-                }
-
-            }
-                    
-
-        }
-
-        // ----------------------------------------------------- VALID_VALUES.JSON ----------------------------------------------------------
-
+      
         $amazonCategoryList = $final_all_complete_indexes;
         $valid_values       = $valid_values_array;
 
@@ -505,7 +509,7 @@ class Amazon_Integration_For_Woocommerce_Admin {
 
             foreach ( $amazonCategoryList as $fieldsKey => $fieldsArray ) {
 
-                $select_html2 = $this->prepareProfileFieldsSection( $fieldsKey, $fieldsArray, array(), 'no', $valid_values, 'sub_category_id' );
+                $select_html2 = $this->prepareProfileFieldsSection( $fieldsKey, $fieldsArray, array(), 'no', $valid_values, $sub_category_id );
 
                 if ( $select_html2['display_heading'] ) {
                     $select_html .= '<tr class="categoryAttributes" ><td colspan="3"></td></tr>
@@ -580,7 +584,7 @@ class Amazon_Integration_For_Woocommerce_Admin {
 
                                 } else {
 
-                                    $prodileRowHTml      = $this->prepareProfileRows( array(), 'no', $valid_values, 'sub_category_id', '', '', $fieldsKey2, $fieldsValue[0], 'yes', '', '','' );
+                                    $prodileRowHTml      = $this->prepareProfileRows( array(), 'no', $valid_values, $sub_category_id, '', '', $fieldsKey2, $fieldsValue[0], 'yes', '', '','' );
                                     $optionalFieldsHtml .= $prodileRowHTml;
                                 }
                             }
