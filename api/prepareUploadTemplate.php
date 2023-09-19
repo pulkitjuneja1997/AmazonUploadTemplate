3fd479489abb638c3bffd4e1d07ef03c5dafa6de
pulkitjuneja-1216 <pulkitjuneja@cedcommerce.com>
2023-09-19 12:24:48 +0530

    V 2.0

--- api/prepareUploadTemplate.php
+++ api/prepareUploadTemplate.php
@@ -32,8 +32,8 @@ class Amazon_Integration_For_Woocommerce_Admin {
 	*/
 	public function __construct() {
 
-        error_reporting(~0);
-		ini_set('display_errors', 1);
+        // error_reporting(~0);
+		// ini_set('display_errors', 1);
 
 		$this->reader = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
 
