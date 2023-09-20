97dd013bb5d4fb6b600689b85f13f771ffc93a85
pulkitjuneja-1216 <pulkitjuneja@cedcommerce.com>
2023-09-20 11:14:44 +0530

    V 2.0

--- api/singleprepareUploadTemplate.php
+++ api/singleprepareUploadTemplate.php
@@ -349,26 +349,16 @@ class Amazon_Integration_For_Woocommerce_Admin {
 			),
 		);  
 		
-		if( 0 == $request_body['rowNum'] ){
+		
 
-			// session_destroy();
-			// session_start();
+		$fileContents = file_get_contents($fileUrl, false, stream_context_create($arrContextOptions));
+		$localFileName = tempnam(sys_get_temp_dir(), 'tempxls');
 
-			$fileContents = file_get_contents($fileUrl, false, stream_context_create($arrContextOptions));
-			$localFileName = tempnam(sys_get_temp_dir(), 'tempxls');
+		// $this->session = [];
+		file_put_contents($localFileName, $fileContents );
+		$this->session['localFileName'] = $localFileName;
 
-			// $this->session = [];
-		    file_put_contents($localFileName, $fileContents );
-			$this->session['localFileName'] = $localFileName;
 
-		} else{
-			// session_start();
-			// $this->session = 
-			// var_dump($this->session);
-
-			// var_dump($this->rowNum);
-			$localFileName = $this->session['localFileName'];
-		}
 
 		$this->reader->setLoadAllSheets();
 		$this->reader->setReadDataOnly(true);
